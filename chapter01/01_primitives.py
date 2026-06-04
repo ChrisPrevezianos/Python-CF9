209e1707d43202560ce71ff073fb02f326eceb17
ChrisPrevezianos <chrisprevezianos@gmail.com>
2026-06-04 23:09:50 +0300

    feat(learning): Primitive Data Types

---
+++ chapter01/01_primitives.py
@@ -0,0 +1,21 @@
+num = 10
+
+height = 1.82
+
+is_raining = False
+
+message = "I love Python dev"
+
+print(num)
+print(height)
+print(is_raining)
+print(message)
+
+print("--------------------")
+
+print(num, height, is_raining, message)
+
+print()
+
+print(num, height, is_raining, message, sep="-")
+ 
